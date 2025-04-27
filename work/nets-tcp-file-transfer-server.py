341975f935be5887fd4929fa9e703fbfdda3a568
jason trieu <jltrieu@miners.utep.edu>
2025-04-27 09:55:29 -0600

    adds forking

--- work/nets-tcp-file-transfer-server.py
+++ work/nets-tcp-file-transfer-server.py
@@ -37,16 +37,40 @@ def readFraming(connected):
         file_size -= len(data)
     
     os.close(fdOut)
+    connected.shutdown(socket.SHUT_WR)
+    sys.exit(0)
+    
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print(f"server listening at ip address {listenAddr} and port {listenPort}")
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.settimeout(5) # block for no more than 5 secs
 s.bind((listenAddr, listenPort))
 s.listen(1) # only one request
 
-conn, addr = s.accept()
-print('Connected by ', addr)
-
-readFraming(conn)
-
-conn.shutdown(socket.SHUT_WR)
-conn.close()
+while True:
+    while pidAddr.keys():
+        if(waitResult := os.waitid(os.P_ALL, 0, os.WNOHANG | os.WEXITED)):
+            zPid, zStatus = waitResult.si_pid, waitResult.si_status
+            del pidAddr[zPid]
+        else:
+            break
+    
+    try:
+        connSockAddr = s.accept()
+        print('Connected by ', connSockAddr)
+    except TimeoutError:
+        connSockAddr = None
+    
+    if connSockAddr is None:
+        continue
+    
+    conn, addr = connSockAddr
+    forkResult = os.fork()
+    if (forkResult == 0):
+        s.close() # child doesnt need this.
+        readFraming(conn)
+    
+    conn.close()
+    pidAddr[forkResult] = addr
+    print(f"spawned off child with pid = {forkResult} at addr {addr}")
